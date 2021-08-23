3b389679ebc48b6a28b820b250b730416141ef97
Andy <andyjhorn@gmail.com>
2021-08-23 16:53:40 -0500

    refactored to simplify instantiation and usage

--- Debounce/Debouncer.cs
+++ Debounce/Debouncer.cs
@@ -11,6 +11,7 @@ namespace Debounce
     {
         private readonly Action<object> _action;
         private readonly object _parameter;
+        private readonly int _interval;
         private DispatcherTimer _timer;
 
         /// <summary>
@@ -18,29 +19,38 @@ namespace Debounce
         /// </summary>
         /// <param name="action">The <see cref="Action"/> to be executed.</param>
         /// <param name="parameter">An optional parameter for the action.</param>
-        public Debouncer(Action<object> action, object parameter = null)
+        public Debouncer(Action<object> action, int intervalMs, object parameter = null)
         {
             _action = action;
+            _interval = intervalMs;
             _parameter = parameter;
         }
 
         /// <summary>
+        /// Gets or sets the dispatcher priority.
+        /// </summary>
+        public DispatcherPriority Priority { get; set; }
+            = DispatcherPriority.ApplicationIdle;
+
+        /// <summary>
+        /// Gets or sets the dispatcher used to invoke the function.
+        /// </summary>
+        public Dispatcher Dispatcher { get; set; }
+
+        /// <summary>
         /// Sets up the debounce interval for the supplied <see cref="Action"/>.
         /// </summary>
-        /// <param name="intervalMs">The number of milliseconds that must elapse before the action is invoked.</param>
-        /// <param name="priority">The priority at which the dispatcher is set to invoke the action.</param>
-        /// <param name="dispatcher">The dispatcher which will invoke the action; will use the current dispatcher if none is supplied.</param>
-        public void Debounce(int intervalMs, DispatcherPriority priority = DispatcherPriority.ApplicationIdle, Dispatcher dispatcher = null)
+        public void Debounce()
         {
             // Stop any existing timer - this is the core of the "debounce" functionality.
             _timer?.Stop();
             _timer = null;
 
             // Use the "current" dispatcher if none was provided.
-            dispatcher ??= Dispatcher.CurrentDispatcher;
+            Dispatcher ??= Dispatcher.CurrentDispatcher;
 
             // Create a new timer to invoke the action after the timer has expired.
-            _timer = new DispatcherTimer(TimeSpan.FromMilliseconds(intervalMs), priority, OnExecute, dispatcher);
+            _timer = new DispatcherTimer(TimeSpan.FromMilliseconds(_interval), Priority, OnExecute, Dispatcher);
             _timer.Start();
         }
 
